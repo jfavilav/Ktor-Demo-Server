17f19e64a6ed6a22726e1a9d73bc151981b12942
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-10 10:45:23 -0500

    Plugin added

--- src/main/kotlin/jairoavila/com/plugins/Routing.kt
+++ src/main/kotlin/jairoavila/com/plugins/Routing.kt
@@ -1,11 +1,12 @@
 package jairoavila.com.plugins
 
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
-
+    install(CallLogging)
     routing {
         get("/") {
             call.respondText("Hello World!")
