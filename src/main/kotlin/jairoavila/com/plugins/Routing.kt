751106965c83fc0d88b18f91ebcd4669f8877ac3
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-11 17:12:51 -0500

    Templating added

--- src/main/kotlin/jairoavila/com/plugins/Routing.kt
+++ src/main/kotlin/jairoavila/com/plugins/Routing.kt
@@ -1,14 +1,39 @@
 package jairoavila.com.plugins
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
-
+    install(CallLogging)
     routing {
+        static {
+            resources("assets")
+        }
+
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/welcome") {
+            val name = call.request.queryParameters["name"]
+            call.respondHtml {
+                head {
+                    title { +"Custom Title" }
+                }
+                body {
+                    if(name.isNullOrEmpty()) {
+                        h3 { +"Welcome" }
+                    } else {
+                        h3 { +"Welcome $name" }
+                    }
+                    img(src = "img.png")
+                }
+            }
         }
     }
 }
