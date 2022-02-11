88f05a295bf5b7bca165503cf04979a974c72f6e
Jairo Avila <jairo.avila@mercadolibre.com.co>
2022-02-11 16:56:14 -0500

    static content added

--- src/main/kotlin/jairoavila/com/plugins/Routing.kt
+++ src/main/kotlin/jairoavila/com/plugins/Routing.kt
@@ -1,12 +1,18 @@
 package jairoavila.com.plugins
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
-
+    install(CallLogging)
     routing {
+        static(remotePath = "assets") {
+            resources("static")
+        }
+
         get("/") {
             call.respondText("Hello World!")
         }
